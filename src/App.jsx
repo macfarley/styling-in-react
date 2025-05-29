9ffca1bce499d799911d07351da5386254263521
macfarley90 <therealmccoyster@gmail.com>
2025-05-29 18:14:05 -0400

    Added button components, reset Css

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      {
+        // Add all example components:
+        <Button1 buttonText="Example 1" />
+        //...
+      }
+    </div>
+  );
+};
+
+export default App;
